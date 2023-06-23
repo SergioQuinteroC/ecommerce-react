167b732b021e88a98a522c0ad9c6dd67c8959112
Sergio Quintero <sergio.quinteroc@udea.edu.co>
2023-06-23 18:20:29 -0500

    consumo de api

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,23 @@
+import { useEffect, useState } from "react";
 import Card from "../../Components/Card";
 
 function Home() {
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    fetch("https://api.escuelajs.co/api/v1/products").then((response) =>
+      response.json().then((data) => setItems(data))
+    );
+  }, []);
+
   return (
     <>
       Home
-      <Card />
+      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+        {items?.map((item) => (
+          <Card key={item.id} {...item} />
+        ))}
+      </div>
     </>
   );
 }
